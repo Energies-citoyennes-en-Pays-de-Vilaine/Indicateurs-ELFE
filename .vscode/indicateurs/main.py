256dd473e12960e4b7cebd5f01892f77e7702cbe
Liana <liana.blandin@insa-rennes.fr>
2023-05-26 12:18:09 +0200

    creation package

---
+++ .vscode/indicateurs/main.py
@@ -0,0 +1,9 @@
+from indicateurs.connectionBDD import connectionBDD
+
+def main():
+    test_connection(bdd1)
+    test_connection(bdd2)
+    test_connection(bdd3)
+
+if __name__ == "__main__":
+    sys.exit(main())
