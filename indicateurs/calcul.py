be5b7a3ebd7d67bfe0db98657576be19a06c850c
Liana <liana.blandin@insa-rennes.fr>
2023-06-21 12:57:12 +0200

    début déploiement

--- indicateurs/calcul.py
+++ indicateurs/calcul.py
@@ -17,3 +17,33 @@ print("Count:", result)
 def nb_appareils_pilotes():
     return db.select([result]).group_by(result.c.first_valid_timestamp).order_by(db.desc(result.c.first_valid_timestamp))
 """
+
+import asyncio
+from connectionBDD import *
+from connectionZabbix import *
+from datetime import datetime
+import sys
+import pandas as pd
+import os
+import subprocess
+import shutil
+import csv
+
+
+#Calcul du nombre d'appareils connectés par 1/4h
+def indic_appareils_connectes() -> int : 
+    conn_sortie = ConnectionBDD('preprod_sortie_ems') 
+    with conn_sortie.engine.connect() as conn:
+        appconn = pd.read_sql("SELECT COUNT(*) FROM result\
+                                    GROUP BY first_valid_timestamp\
+                                    ORDER BY first_valid_timestamp DESC\
+                                    LIMIT 1"\
+                                    , con = conn)
+    return appconn
+        
+    #Calcul du nombre d'appareils pilotés la semaine dernière
+def indic_appareils_pilotes_semaine() -> int :
+        with conn_sortie.engine.connect() as conn:
+            appsem = pd.read_sql("SELECT COUNT(DISTINCT machine_id) AS Nombre_appareils_pilotés_la_semaine_dernière FROM result\
+                                    WHERE first_valid_timestamp > (CAST(EXTRACT (epoch FROM NOW()) AS INT) - 604800)", con = conn)
+        return appsem
